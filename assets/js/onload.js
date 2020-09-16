79d88eee48319ceef5c80f10deec440ce46a01a8
therogerize <roygutierrezfranco@gmail.com>
2020-09-16 01:10:38 -0400

    Formulario de saludos funcionando

--- assets/js/onload.js
+++ assets/js/onload.js
@@ -4,16 +4,16 @@ $(document).ready(function () {
         const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i
         return expression.test(String(email).toLowerCase())
     }
+    //Nombre
+    const inputNombre = $("#name");
+    const inputCorreo = $("#email");
+    const inputMotivo = $("#messageid");
+    const inputMensaje = $("#message");
 
     function validate(dato, tipo) {
-        //Nombre
-        const inputNombre = $("#name");
-        const inputCorreo = $("#email");
-        const inputMotivo = $("#messageid");
-        const inputMensaje = $("#message");
+
         switch (tipo) {
             case "nombre":
-                console.log("entrando en nombre 2")
                 if (dato == "") {
                     $("div.nombreError").fadeIn(300);
                     $("div.nombreError").text("Debes ingresar un nombre y apellido.");
@@ -59,14 +59,14 @@ $(document).ready(function () {
                     errors++;
                     break;
                 }
-                else if (dato.length > 300) {
+                else if (dato.length > 500) {
                     $("div.emailError").fadeIn(300);
                     $("div.emailError").text("El correo electrónico ingresado es demasiado extenso.");
                     $(inputCorreo).addClass("invalid");
                     errors++;
                     break;
                 }
-                else if (dato.length > 0 && dato.length < 300 && validateEmail(dato)) {
+                else if (dato.length > 0 && dato.length < 500 && validateEmail(dato)) {
                     $("div.emailError").fadeOut(300);
                     $("div.emailError").text("");
                     $(inputCorreo).removeClass("invalid");
@@ -103,7 +103,7 @@ $(document).ready(function () {
                     errors++;
                     break;
                 }
-                else if (dato.length > 0 && dato.length < 100 && dato !== "") {
+                else if (dato.length > 0 && dato.length < 2000 && dato !== "") {
                     $("div.mensajeError").fadeOut(300);
                     $("div.mensajeError").text("");
                     $(inputMensaje).removeClass("invalid");
@@ -114,13 +114,18 @@ $(document).ready(function () {
     }
     function validateForm(nombre, motivo, email, mensaje) {
         errors = 0;
-        console.log("entrando en nombre 1: " + nombre);
         validate(nombre, 'nombre');
         validate(email, 'email');
         validate(motivo, 'motivo');
         validate(mensaje, 'mensaje');
-        console.log("Aqui los errores: " + errors);
         return errors;
+    }
+    function cleanForm() {
+        $(inputCorreo).val("");
+        $(inputNombre).val("");
+        $(inputMotivo).val("");
+        $(inputMotivo).prop('selectedIndex', 0);
+        $(inputMensaje).val("");
     }
     $("#form-submit").on('click', function (e) {
 
@@ -130,6 +135,7 @@ $(document).ready(function () {
         const mensaje = $("#message").val();
         const arrayy = [nombre, motivo, email, mensaje];
         console.log(arrayy);
+        //TEMPORAL PARA PRUEBAS DE BACK
         validateForm(nombre, motivo, email, mensaje);
         if (errors == 0) {
             const formData = {
@@ -140,24 +146,33 @@ $(document).ready(function () {
             }
             console.log(formData);
             e.preventDefault();
-            console.log("Aqui vamos")
             $.ajax({
                 type: "POST",
+                crossDomain: true,
+                async: true,
                 url: 'correo.php',
                 data: formData,
-                success: function (response) {
-                    var jsonData = response;
-                        console.log("Aqui: " + jsonData);
-                        if (jsonData.success == "1") {
-                        console.log(jsonData);
-
+                dataType: 'json',
+                success: function (result) {
+                    if (result[0].error) {
+                        console.log(result[0].error);
+                        $("#errorMsg").fadeIn(300);
+                        $("#errorMsg").text(result[0].error);
                     }
-                    else {
-                        alert('Invalid Credentials!');
+                    if (result[0].success) {
+                        console.log(result[0].success);
+                        $("#errorMsg").fadeOut(0);
+                        $("#submitButton").fadeOut(0);
+                        $("#successMsg").fadeIn(300);
+                        cleanForm();
                     }
+                    // see attached image how to get the path for object
+                },
+                error: function (result) {
+                    alert("Ocurrió un error con el servidor, intente más tarde");
                 }
             });
-        } 
+        }
         // else {
         //     alert("Se encontraron errores")
         // }
--- correo.php
+++ correo.php
@@ -2,8 +2,68 @@
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 //$link = mysqli_connect("localhost", "emgagenc_lmdlc", ";e9*(M0KP3jy", "emgagenc_losmdlc");
+function set_Motivo($motivo) {
+    $selected = "";
+    switch($motivo) {
+        case "1": 
+            $selected = utf8_decode("Cumpleaños");
+        break;
+        case "2": 
+            $selected = "Saludo Personalizado";
+        break;
+    }
+    return $selected;
+}
+function validateForm($name, $email, $motivo, $message) {
+    $returnErrors = [];
+    $nameLength = strlen($name);
+    $emailLength = strlen($email);
+    $motivoLength = strlen($motivo);
+    $messageLength = strlen($message);
+    $errors = 0;
+    $returnErrors[] = array(
+        "error" => "Se encontraron errores en el formulario.");
+    //Nombre
+    if($name == "" || $nameLength > 0 && $nameLength < 5 ) {
+        $errors++;
+    }
+    else if($nameLength > 100) {
+        $errors++;
+    }
+    
+    //Motivo
+    if($motivo == "" || $motivoLength == 0) {
+        $errors++;
+    }
+    if($motivo !== "1" &&  $motivo !== "2") {
+        $errors++;
+    }
+
+
+    //Mensaje
+    if($message == "" || $messageLength == 0) {
+        $errors++;
+    }
+    else if($messageLength > 2000) {
+        $errors++;
+    }
+    //Email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors++;
+    }
+    if($email == "" || $emailLength == 0) {
+        $errors++;
+    }
+    else if($emailLength > 500) {
+        $errors++;
+    }
+    if($errors > 0) {
+        echo json_encode($returnErrors);
+        exit;
+    } 
+}
+
 $link = mysqli_connect("localhost", "root", "Admin123", "emgagenc_losmdlc");
-//  echo "entrando";
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
@@ -19,37 +79,48 @@ $geoPluginarray = unserialize( file_get_contents('http://www.geoplugin.net/php.g
 $paisdata = $geoPluginarray['geoplugin_countryName'];
 $curso = "Saludo de los meniques"; 
 // $select = $_POST['select'];
-$name = mysqli_real_escape_string($link, $_POST['name']);
-$email = mysqli_real_escape_string($link, $_POST['email']);
-$messageid = mysqli_real_escape_string($link, $_POST['messageid']);
-$message = mysqli_real_escape_string($link, $_POST['message']);
+// $name = mysqli_real_escape_string($link, $_POST['name']);
+// $email = mysqli_real_escape_string($link, $_POST['email']);
+// $messageid = mysqli_real_escape_string($link, $_POST['messageid']);
+// $motivoText = set_Motivo($messageid);
+// $message = mysqli_real_escape_string($link, $_POST['message']);
+// $pais = mysqli_real_escape_string($link, $paisdata);
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$messageid = $_POST['messageid'];
+$motivoText = set_Motivo($messageid);
+$message = $_POST['message'];
 $pais = mysqli_real_escape_string($link, $paisdata);
-// var_dump($name);
-// var_dump($email);
-// var_dump($message);
-// var_dump($messageid);
+validateForm($name, $email, $messageid, $message);
 
 // Attempt insert query execution
 // $sql = "INSERT INTO cliente (name, email, pais, messageid, message) VALUES ('$name', '$email', '$pais', '$messageid', '$message')";
 $sql = "INSERT INTO cliente (name, email, pais, messageid, message) VALUES (?, ?, ?, ?, ?)";
 $stmt = $link->prepare($sql);
-$stmt->bind_param("sssss", $name, $email, $pais, $messageid, $message);
+$stmt->bind_param("sssss", $name, $email, $pais, $motivoText, $message);
 if($stmt->execute()) {
-    echo "Success";
+    // echo "Success";
     $destinatario = "losmeniquesdelacasa@gmail.com";
     $asunto = "$curso";
     $carta = "Nombre: $name \n";
     $carta .= "Correo: $email \n";
-    $carta .= "Motivo: $messageid \n";
+    $carta .= "Motivo: $motivoText \n";
     $carta .= "Mensaje: $message \n";
     $carta .= "Ubicacion: $paisdata";
-
+    $success[] = array(
+        "success" => "success");
+        echo json_encode($success);
 // mail($destinatario, $asunto, $carta);
-echo "CORREO ENVIADO";
+// echo "CORREO ENVIADO";
 //header('location:https://losmeniquesdelacasa.com');
      exit();
 } else {
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+    $returnErrors[] = array(
+    "error" => "ERROR: Could not able to execute $sql. '".mysqli_error($link)."'" );
+    echo json_encode($returnErrors);
+    exit;
+    // echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
 
 //Close connection
